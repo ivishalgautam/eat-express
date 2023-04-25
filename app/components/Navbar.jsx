61ec1f4921bca4e585cc2182b33408ee0285751f
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-04-25 16:29:42 +0530

    made it better

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -4,10 +4,27 @@ import React, { useEffect, useRef, useState } from "react";
 import { BsCart2 } from "react-icons/bs";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
+
+let navList = [
+  {
+    name: "home",
+    href: "/home",
+  },
+  {
+    name: "menu",
+    href: "/menu",
+  },
+  {
+    name: "about",
+    href: "/about",
+  },
+];
 
 const Navbar = () => {
   const [prevScrollPos, setPrevScrollPos] = useState(0);
   const [isVisible, setIsVisible] = useState(true);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -28,11 +45,12 @@ const Navbar = () => {
     let element = document.documentElement;
     element.classList.toggle("dark");
   };
+
   return (
     <div
       className={`w-full fixed ${
         isVisible ? "translate-y-0" : "-translate-y-16"
-      } px-4 sm:px-14 md:px-20 lg:px-24 bg-indigo-600 dark:bg-gray-900 backdrop-blur-lg drop-shadow-xl text-slate-100 flex items-center justify-between h-16 transition-all`}
+      } px-4 sm:px-14 md:px-20 lg:px-24 bg-indigo-500 dark:bg-gray-900 backdrop-blur-lg drop-shadow-xl text-slate-100 flex items-center justify-between h-16 transition-all`}
     >
       <div className={`logo text-xl font-bold tracking-wider`}>
         <Link href="/">
@@ -40,13 +58,19 @@ const Navbar = () => {
           <span className="text-gray-900 dark:text-indigo-500">express</span>
         </Link>
       </div>
+
       <nav className="ml-auto mr-10 hidden md:block">
         <ul className="flex items-center justify-center gap-5">
-          <li>Home</li>
-          <li>Menu</li>
-          <li>About Us</li>
+          {navList.map((navItem, key) => {
+            return (
+              <li key={key} className="capitalize">
+                <Link href={navItem.href}>{navItem.name}</Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
+
       <div className="flex items-center justify-center gap-0 md:gap-2">
         <div className="btn-header">
           <Link href="/cart">
@@ -59,9 +83,34 @@ const Navbar = () => {
         >
           <MdOutlineDarkMode size={20} />
         </button>
-        <button className="btn-header flex md:hidden">
+        <button
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+          className="btn-header flex md:hidden"
+        >
           <RxHamburgerMenu size={20} />
         </button>
+      </div>
+
+      <div
+        className={`absolute ${
+          showMobileMenu ? "right-0" : "-right-full"
+        } top-0 h-screen w-80 bg-indigo-500 transition-all block md:hidden`}
+      >
+        <button
+          className="btn-header absolute top-1 right-1"
+          onClick={() => setShowMobileMenu(!showMobileMenu)}
+        >
+          <IoMdClose size={30} />
+        </button>
+        <ul className="flex flex-col items-center justify-center mt-20 gap-10">
+          {navList.map((navItem, key) => {
+            return (
+              <li key={key} className="capitalize text-xl">
+                <Link href={navItem.href}>{navItem.name}</Link>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
